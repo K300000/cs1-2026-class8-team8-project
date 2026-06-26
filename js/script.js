fa03ae08366552209f54c8b6799535dba815b56a
K300000 <s1f102602958@iniad.org>
2026-06-26 17:41:32 +0900

    スライドのシステムを変更
    左右に遷移するボタンと今の画像が何番目かを確認できる(●〇〇)を途中まで作成

--- js/script.js
+++ js/script.js
@@ -2,7 +2,44 @@ const slides = document.querySelector(".slides");
 
 slides.style.transform = "translateX(0%)";
 
+let currentIndex = 0;
+
 function moveSlide(index){
+    currentIndex = index;
     slides.style.transform =
         `translateX(-${index * 100}%)`;
+    updateDots();
+}
+
+function updateDots(){
+
+    const dots = document.querySelectorAll(".dot");
+
+    dots.forEach(dot=>{
+        dot.classList.remove("active");
+    });
+
+    dots[currentIndex].classList.add("active");
+}
+
+function nextSlide(){
+
+    currentIndex++;
+
+    if(currentIndex > 2){
+        currentIndex = 0;
+    }
+
+    moveSlide(currentIndex);
+}
+
+function prevSlide(){
+
+    currentIndex--;
+
+    if(currentIndex < 0){
+        currentIndex = 2;
+    }
+
+    moveSlide(currentIndex);
 }
